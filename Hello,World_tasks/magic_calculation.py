6db9a2272b7d4dcfec856f36274dbde1f07fcf98
arkoaikins <arkoaikins@gmail.com>
2023-06-02 11:47:24 +0000

    added comments

--- Hello,World_tasks/magic_calculation.py
+++ Hello,World_tasks/magic_calculation.py
@@ -1,7 +1,9 @@
 from dis import dis
+"""dis helps to view python bytecode"""
 
 def magic_calculation(a, b):
     return (98 + a ** b)
-
+"""that is the function we want to see its bytecode"""
 
 dis(magic_calculation)
+""" this is how to use the dis function to run the bytcode"""
