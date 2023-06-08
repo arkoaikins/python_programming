a6f3274cbbe8ba52cf0041013a6691d6ae8ffbfd
arkoaikins <arkoaikins@gmail.com>
2023-06-08 15:13:55 +0000

    just experimenting if else

---
+++ if_else,loops,functions/intro_to_ifelse.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(1, 10):
+    if i <= 5:
+        print("AAAAAHHHHH")
+    elif i >= 6 and i <= 8:
+        print("ARE ALL THE BRACKETS???")
+    else:
+        print("DO YOU PEOPLE READ THIS SYNTAX EASILY")
