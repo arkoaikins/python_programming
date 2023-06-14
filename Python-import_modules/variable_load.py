df674f6be7062880e8c5ee4b39187ef45642ac6f
arkoaikins <arkoaikins@gmail.com>
2023-06-14 15:11:28 +0000

    Create a program that prints a number in a module

---
+++ Python-import_modules/variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a program that imports the variable a from the file variable_load_5.py
+and prints its value
+
+"""
+if __name__ == "__main__":
+    """prevents code below to be executed when imported"""
+    from variable_load_5 import a
+    print(a)
