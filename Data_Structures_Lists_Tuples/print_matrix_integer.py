b8f91fce30e0dab239ddf0c4d0aa77e6fab11c63
arkoaikins <arkoaikins@gmail.com>
2023-07-08 11:32:02 +0000

    create function that prints a matrix

--- Data_Structures_Lists_Tuples/print_matrix_integer.py
+++ Data_Structures_Lists_Tuples/print_matrix_integer.py
@@ -1,17 +1,11 @@
-!/usr/bin/python3
+#!/usr/bin/python3
+#function that prints a matrix of integers.
 def print_matrix_integer(matrix=[[]]):
-    """
-    prints a matrix of integers
-    Args:
-        matrix: a list of lists of integers
-    """
-    # Get the number of rows and columns
-    num_rows = len(matrix)
-    num_col = len(matrix[0])
-
-    for i in range(num_rows):
-        for j in range(num_col):
+    row_numb = len(matrix)
+    colm_numb = len(matrix[0])
+    for i in range(row_numb):
+        for j in range(colm_numb):
             print("{:d}".format(matrix[i][j]), end="")
-            if j < num_col - 1:
+            if j < colm_numb - 1:
                 print(" ", end="")
         print()
